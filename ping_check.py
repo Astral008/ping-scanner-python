5f4f7eead819af5e54a72232df007e3fdad06e5d
astral <michael300830@gmail.com>
2026-04-02 11:02:23 -0500

    feat: add cross-plataform support and error handling for ping
    -Detect OS (windows/Linux) to use correct ping parameters.
    
    Add timeout and return code checks for more reliable results.
    
    Improve file reading with UTF-8 encodig and empty line skipping

--- ping_check.py
+++ ping_check.py
@@ -1,22 +1,25 @@
 import subprocess
 import ipaddress
+import platform
 
 
-list_ip = []
-# funciones
+
 def read_file(file_path):
-	
+	list_ip = []
 	try:
-		with open(file_path, 'r') as file:
+		with open(file_path, 'r', encoding='utf-8') as file:
 			for line in file:
 				ip_encontrada=line.strip()
+				if not ip_encontrada:
+					continue
 				if check_ip(ip_encontrada):
 					list_ip.append(ip_encontrada)
 				else:
-					print(f"Dirección IP no válida encontrada en el archivo: {ip_encontrada}")
+					print(f"🔴 Dirección IP no válida encontrada en el archivo: {ip_encontrada}")
 		return list_ip
 	except FileNotFoundError:
 		print("El archivo no se encontró.")
+		return []
 
 def check_ip(dirrec_ip):
 	
@@ -29,18 +32,24 @@ def check_ip(dirrec_ip):
 		
 def run_process(ips_prueba):
 
+	parametro_ping = "-n" if platform.system().lower() == "windows" else "-c"
+
 	for ip_unica in ips_prueba:
 		print(f"Ejecutando ping para la dirección IP: {ip_unica}")
 		try:
-			resultado=subprocess.run (["ping","-c","2" ,ip_unica],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			print (resultado.stdout)
+			resultado=subprocess.run (["ping", parametro_ping, "2", ip_unica],stdout=subprocess.PIPE, stderr=subprocess.PIPE,timeout=5, text=True)
 		except subprocess.CalledProcessError as e:
 			print(f"Error: {e.stderr.decode()}")
+		except subprocess.TimeoutExpired:
+			print(f"🔴 Tiempo de espera agotado para {ip_unica}")
+			continue
+		if resultado.returncode == 0:
+				print(f"🟢 Ping exitoso para {ip_unica}")
+		else:
+				print(f"🔴 Ping fallido para {ip_unica}")
 
-	
 
-#PROGRAMA 
-
-
-result_ip=read_file("hosts.txt")
-run_process(result_ip)
+if __name__ == "__main__":
+	result_ip=read_file("hosts.txt")
+if result_ip:	
+	run_process(result_ip)
